bd819832020c09059a4a783d5e3706036d976a05
madebyjustinzhang <jzha365@uwo.ca>
2020-01-18 14:40:05 -0500

    Added animations

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FadeText from './components/FadeText.js';
 
-import  FadeText from './components/FadeText.js';
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
-    <div className="App">
+    <div className="main">
+        <h1>Time Block<br />Your Life</h1>
+        <br></br>
         <FadeText
-            text="
-
-            "/>
+            title="What Is Time Blocking?"
+            content="
+            Why put tasks in a to-do list and forget to do them, when you can schedule them automatically in your calendar?
+            "
+            image="peopleworking.png"
+        />
     </div>
   );
 }
--- src/components/FadeText.js
+++ src/components/FadeText.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 
-class FadeExample extends React.Component {
+class FadeText extends React.Component {
   render() {
     return (
       <div>
-        <Fade left>
-          <h1>React Reveal</h1>
+        <Fade up>
+          <h2>{this.props.title}</h2>
+        </Fade>
+
+        <Fade up>
+          <p>{this.props.content}</p>
+        </Fade>
+
+        <Fade right>
+            <img src={"../public/images/" + this.props.image} />
         </Fade>
       </div>
     );
   }
 }
 
-export default FadeExample;
+export default FadeText;
