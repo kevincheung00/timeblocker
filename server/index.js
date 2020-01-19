34923c58864f779357db0a44765f84caefa5e90f
madebyjustinzhang <jzha365@uwo.ca>
2020-01-19 05:30:30 -0500

    nice

--- server/index.js
+++ server/index.js
@@ -12,6 +12,25 @@ app.get('/api/greeting', (req, res) => {
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
+var myPythonScriptPath = './src/calendar/retrieveData.py';
+
+// Use python shell
+var PythonShell = require('python-shell');
+var pyshell = new PythonShell(myPythonScriptPath);
+
+pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    console.log(message);
+});
+
+// end the input stream and allow the process to exit
+pyshell.end(function (err) {
+    if (err){
+        throw err;
+    };
+
+    console.log('finished');
+});
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
